b66a443f559d9adc51736c263fd58bcdd4868eb0
Matt9172 <56167552+Matt9172@users.noreply.github.com>
2020-02-21 15:02:13 +0000

    Add files via upload

---
+++ P1SelectorScreen/src/selectorPage/SelectorController.java
@@ -0,0 +1,23 @@
+package selectorPage;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class SelectorController {
+
+
+    public ListView ExpertList;
+    public Button ConnectButton;
+    public TextField SearchBar;
+
+    public void searchForExperts(ActionEvent actionEvent) {
+
+
+    }
+
+    public void connectToExpert(ActionEvent actionEvent) {
+
+    }
+}
